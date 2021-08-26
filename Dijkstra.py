e1a889402d2defc3b39d0dca7d2ead4840ef4d2f
Vandana Muppavarapu <61150709+vandana002@users.noreply.github.com>
2021-08-26 09:58:54 +0530

    Update Dijkstra.py

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,55 +1,56 @@
 import sys
-vertices = [[0, 0, 1, 1, 0, 0, 0],
-[0, 0, 1, 0, 0, 1, 0],
-[1, 1, 0, 1, 1, 0, 0],
-[1, 0, 1, 0, 0, 0, 1],
-[0, 0, 1, 0, 0, 1, 0],
-[0, 1, 0, 0, 1, 0, 1],
-[0, 0, 0, 1, 0, 1, 0]]
-edges = [[0, 0, 1, 2, 0, 0, 0],
-[0, 0, 2, 0, 0, 3, 0],
-[1, 2, 0, 1, 3, 0, 0],
-[2, 0, 1, 0, 0, 0, 1],
 
-[0, 0, 3, 0, 0, 2, 0],
-[0, 3, 0, 0, 2, 0, 1],
-[0, 0, 0, 1, 0, 1, 0]]
-# Find which vertex is to be visited next
+# Function to find out which of the unvisited node 
+# needs to be visited next
 def to_be_visited():
-global visited_and_distance
-v = -10
-for index in range(num_of_vertices):
-if visited_and_distance[index][0] == 0 \
-and (v < 0 or visited_and_distance[index][1] <=
-visited_and_distance[v][1]):
-v = index
-return v
+  global visited_and_distance
+  v = -10
+  # Choosing the vertex with the minimum distance
+  for index in range(number_of_vertices):
+    if visited_and_distance[index][0] == 0 \
+      and (v < 0 or visited_and_distance[index][1] <= \
+      visited_and_distance[v][1]):
+        v = index
+  return v
 
-num_of_vertices = len(vertices[0])
+# Creating the graph as an adjacency matrix
+vertices = [[0, 1, 1, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+            [0, 0, 0, 0]]
+edges =  [[0, 3, 4, 0],
+          [0, 0, 0.5, 0],
+          [0, 0, 0, 1],
+          [0, 0, 0, 0]]
+
+number_of_vertices = len(vertices[0])
+
+# The first element of the lists inside visited_and_distance 
+# denotes if the vertex has been visited.
+# The second element of the lists inside the visited_and_distance 
+# denotes the distance from the source.
 visited_and_distance = [[0, 0]]
-for i in range(num_of_vertices-1):
-visited_and_distance.append([0, sys.maxsize])
-for vertex in range(num_of_vertices):
-# Find next vertex to be visited
-to_visit = to_be_visited()
-for neighbor_index in range(num_of_vertices):
-# Updating new distances
-if vertices[to_visit][neighbor_index] == 1 and \
-visited_and_distance[neighbor_index][0] == 0:
-new_distance = visited_and_distance[to_visit][1] \
-+ edges[to_visit][neighbor_index]
-if visited_and_distance[neighbor_index][1] >
-new_distance:
-visited_and_distance[neighbor_index][1] =
-new_distance
-visited_and_distance[to_visit][0] = 1
-i = 0
-# Printing the distance
-for distance in visited_and_distance:
-print("Distance of ", chr(ord('a') + i),
-" from source vertex: ", distance[1])
-i = i + 1
+for i in range(number_of_vertices-1):
+  visited_and_distance.append([0, sys.maxsize])
 
-2)Time Complexity: O(E Log V)
-where, E is the number of edges and V is the number of vertices.
-Space Complexity: O(V)
+for vertex in range(number_of_vertices):
+  # Finding the next vertex to be visited.
+  to_visit = to_be_visited()
+  for neighbor_index in range(number_of_vertices):
+    # Calculating the new distance for all unvisited neighbours
+    # of the chosen vertex.
+    if vertices[to_visit][neighbor_index] == 1 and \
+     visited_and_distance[neighbor_index][0] == 0:
+      new_distance = visited_and_distance[to_visit][1] \
+      + edges[to_visit][neighbor_index]
+      # Updating the distance of the neighbor if its current distance
+      # is greater than the distance that has just been calculated
+      if visited_and_distance[neighbor_index][1] > new_distance:
+        visited_and_distance[neighbor_index][1] = new_distance
+  visited_and_distance[to_visit][0] = 1
+
+i = 0 
+for distance in visited_and_distance:
+  print("The shortest distance of ",chr(ord('a') + i),\
+  " from the source vertex a is:",distance[1])
+  i = i + 1
