ad7c9c1bf5d87122748e07dce7b5051c28d077af
Vandana Muppavarapu <61150709+vandana002@users.noreply.github.com>
2021-08-26 06:59:37 +0530

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -1,78 +1,56 @@
-import random
-import timeit
-import matplotlib.pyplot as plt
-import math
-def partition(list,start,end):
-pivot=list[end]
-x=start
-y=end-1
-global com
-while(1):
-while x<=y and list[x]<=pivot:
-x+=1
-com+=1
-while x<=y and list[y]>=pivot:
-y-=1
-com+=1
-if(x<y):
-list[x],list[y]=list[y],list[x]
-com+=1
-else:
-com+=1
-break
-list[end],list[x]=list[x],list[end]
-
-return x
-def partition_rand(list,start,end):
-randpivot = random.randrange(start,end)
-list[end],list[randpivot]=list[randpivot],list[end]
-return partition(list,start,end)
-def quickSort(list,start,end):
-if start<end:
-z=partition_rand(list,start,end)
-quickSort(list,start,z-1)
-quickSort(list,z+1,end)
-#------------------------------
-timeList=[]
-comList=[]
-for i in range(200):
-com=1
-arr=[]
-for j in range(0,i):
-n = random.randint(1,100)
-list.append(n)
-start = timeit.default_timer()
-quickSort(arr,0,i-1)
-end = timeit.default_timer()
-#print(arr)
-total=end-start
-timeList.append(total)
-comList.append(com)
-timeList = [x * 1000 for x in timeList]
-avgtime=0
-for i in timeList:
-avgtime+=i
-avgtime/=200
-n = [*range(1, 201, 1)]
-nn=[]
-for x in n:
-
-nn.append(math.pow(x,2))
-
-nnn=[]
-for x in n:
-
-nnn.append(x*math.log(x,2))
-
-print("Time required = ",avgtime)
-plt.plot(comList,n,color='yellow', linewidth=3,label='Quicksort time')
-plt.plot(nn,n,color='red', linewidth=3,label='n^2')
-plt.plot(nnn,n,color='blue', linewidth=3,label='nlogn')
-plt.title('Quick Sort Running time')
-plt.xlabel('value of time')
-plt.ylabel('value of n')
-plt.legend()
-plt.show()
+def partition(start, end, array):
+      
+    # Initializing pivot's index to start
+    pivot_index = start 
+    pivot = array[pivot_index]
+      
+    # This loop runs till start pointer crosses 
+    # end pointer, and when it does we swap the
+    # pivot with element on end pointer
+    while start < end:
+          
+        # Increment the start pointer till it finds an 
+        # element greater than  pivot 
+        while start < len(array) and array[start] <= pivot:
+            start += 1
+              
+        # Decrement the end pointer till it finds an 
+        # element less than pivot
+        while array[end] > pivot:
+            end -= 1
+          
+        # If start and end have not crossed each other, 
+        # swap the numbers on start and end
+        if(start < end):
+            array[start], array[end] = array[end], array[start]
+      
+    # Swap pivot element with element on end pointer.
+    # This puts pivot on its correct sorted place.
+    array[end], array[pivot_index] = array[pivot_index], array[end]
+     
+    # Returning end pointer to divide the array into 2
+    return end
+      
+# The main function that implements QuickSort 
+def quick_sort(start, end, array):
+      
+    if (start < end):
+          
+        # p is partitioning index, array[p] 
+        # is at right place
+        p = partition(start, end, array)
+          
+        # Sort elements before partition 
+        # and after partition
+        quick_sort(start, p - 1, array)
+        quick_sort(p + 1, end, array)
+          
+# Driver code
+array = [ 10, 7, 8, 9, 1, 5 ]
+quick_sort(0, len(array) - 1, array)
+  
+print(f'Sorted array: {array}')
+      
 
 
 The best and average complexities are O(nlogn) and for the worst case it is n^2.
