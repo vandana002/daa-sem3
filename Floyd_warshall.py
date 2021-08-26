ee0ed8f6d03a529718ae7bfba3a7f311feeb8a7e
Vandana Muppavarapu <61150709+vandana002@users.noreply.github.com>
2021-08-26 08:05:24 +0530

    Update Floyd_warshall.py

--- Floyd_warshall.py
+++ Floyd_warshall.py
@@ -1,24 +1,20 @@
-no_of_vertices = 4
-INF = 999
+n=4
+INF=99999
 def floyd_warshall(G):
-distance = list(map(lambda i: list(map(lambda j: j, i)), G))
-
-for k in range(no_of_vertices):
-for i in range(no_of_vertices):
-for j in range(no_of_vertices):
-distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
-print_solution(distance)
-
-# Printing the solution
-def print_solution(distance):
-for i in range(no_of_vertices):
-for j in range(no_of_vertices):
-if(distance[i][j] == INF):
-print("INF", end=" ")
-else:
-print(distance[i][j], end=" ")
-print(" ")
-
+    A=list(map(lambda i:list(map(lambda j:j,i)),G))
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                A[i][j]=min(A[i][j],A[i][k]+A[k][j])
+    print_solution(A)
+def print_solution(A):
+    for i in range(n):
+        for j in range(n):
+            if(A[i][j]==INF):
+                print("INF",end=" ")
+            else:
+                print(A[i][j],end=" ")
+        print(" ")
 G = [[0,5,INF,10],
 [INF,0,3,INF],
 [INF,INF,0,1],
