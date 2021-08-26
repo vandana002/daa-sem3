c91c8b0b032256bb40c17140115893f2382a6021
Vandana Muppavarapu <61150709+vandana002@users.noreply.github.com>
2021-08-26 07:35:36 +0530

    Update first pivot.py

--- first pivot.py
+++ first pivot.py
@@ -1,63 +1,27 @@
 // first pivot
 
-def partition(list,start,end):
-pivot=list[start]
-x=start+1
-y=end
-global com
-while True:
-while x<=y and list[x]<=pivot:
-x+=1
-com+=1
-while x<=y and list[y]>=pivot:
-y-=1
-com+=1
-if(x<y):
-list[x],list[y]=list[y],list[x]
-com+=1
-else:
-com+=1
-break
-list[start],list[y]=list[y],list[start]
-return y
+def partition(start, end, array):
+    pivot_index = start 
+    pivot = array[pivot_index]
+    while start < end:
+        while start < len(array) and array[start] <= pivot:
+            start += 1
+        while array[end] > pivot:
+            end -= 1
+        if(start < end):
+            array[start], array[end] = array[end], array[start]
+    array[end], array[pivot_index] = array[pivot_index], array[end]
+    return end
 
-def quickSort(list,start,end):
-if start<end:
-z=partition(list,start,end)
-quickSort(list,start,z-1)
-quickSort(list,z+1,end)
-timeList=[]
-comList=[]
-for i in range(200):
-com=1
-arr=[]
-for j in range(0,i):
-n = random.randint(1,100)
-arr.append(n)
-start = timeit.default_timer()
-quickSort(arr,0,i-1)
-end = timeit.default_timer()
-total=end-start
-timeList.append(total)
-comList.append(com)
-
-timeList = [x * 1000 for x in timeList]
-n = [*range(1, 201, 1)]
-nn=[]
-for x in n:
-
-nn.append(math.pow(x,2))
-
-nnn=[]
-for x in n:
-
-nnn.append(x*math.log(x,2))
-
-plt.plot(comList,n,color='yellow', linewidth=3,label='Quicksort time')
-plt.plot(nn,n,color='red', linewidth=3,label='n^2')
-plt.plot(nnn,n,color='blue', linewidth=3,label='nlogn')
-plt.title('Quick Sort Running time')
-plt.xlabel('value of time')
-plt.ylabel('value of n')
-plt.legend()
-plt.show()
+def quick_sort(start, end, array): 
+    if (start < end):
+      p = partition(start, end, array)
+      quick_sort(start, p - 1, array)
+      quick_sort(p + 1, end, array)
+          
+# Driver code
+array = [ 10, 7, 8, 9, 1, 5 ]
+quick_sort(0, len(array) - 1, array)
+  
+print('Sorted array:',array)
+      
